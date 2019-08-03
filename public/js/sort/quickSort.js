e2fb6624201826d15a78f401325510517c7d9289
JuanLopes <30662756+Truta446@users.noreply.github.com>
2019-08-02 20:43:44 -0400

    Update quickSort.js
    Função quicksort atualizada!

--- public/js/sort/quickSort.js
+++ public/js/sort/quickSort.js
@@ -1,79 +1,61 @@
-/*
-* Quick sort is a comparison sorting algorithm that uses a divide and conquer strategy.
-* For more information see here: https://en.wikipedia.org/wiki/Quicksort
-*/
-function quickSorting(items) {
-
-    var length = items.length;
+/**
+ * Encontrar o pivot no array para comparar com
+ * todos os demais elementos do array.
+ * 
+ * @param {Array} arr 
+ * @param {Int} left 
+ * @param {Int} right 
+ */
+function quickSorting(arr, left = 0, right = arr.length - 1) {
+    let len = arr.length,
+        index;
   
-    if (length <= 1) {
-      return items;
-    }
-    var PIVOT = items[0];
-    var GREATER = [];
-    var LESSER = [];
-  
-    for (var i = 1; i < length; i++) {
-      if (items[i] > PIVOT) {
-        GREATER.push(items[i]);
-      } else {
-        LESSER.push(items[i]);
-      }
-    }
-  
-    var sorted = quickSorting(LESSER);
-    sorted.push(PIVOT);
-    sorted = sorted.concat(quickSorting(GREATER));
+    if(len > 1) {
+        index = partition(arr, left, right);
     
-    return sorted;
+        if(left < index - 1) {
+            quickSorting(arr, left, index - 1);
+        } 
+    
+        if(index < right) {
+            quickSorting(arr, index, right);
+        }
+  
+    }
+  
+    return arr;
   }
+
+  function partition(arr, left, right) {
+    let middle = Math.floor((right + left) / 2),
+        pivot = arr[middle],
+        i = left,
+        j = right;
+  
+    while(i <= j) {
+        while(arr[i] < pivot) {
+            i++;
+        }
+    
+        while(arr[j] > pivot) {
+            j--;
+        }
+    
+        if(i <= j) {
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+            i++;
+            j--;
+        }
+    }
+  
+    return i;
+}
 
 function quickSort(array_numeros) {
     INICIO = performance.now();
     const array_ordenado = quickSorting(array_numeros);
     FIM = performance.now();
+    console.log(`QUICKSORT:${array_ordenado}`);
 
     QUICKSORT = FIM - INICIO;
 }
-
-/* var items = [5,3,7,6,2,9];
-function swap(items, leftIndex, rightIndex){
-    var temp = items[leftIndex];
-    items[leftIndex] = items[rightIndex];
-    items[rightIndex] = temp;
-}
-function partition(items, left, right) {
-    var pivot   = items[Math.floor((right + left) / 2)], //middle element
-        i       = left, //left pointer
-        j       = right; //right pointer
-    while (i <= j) {
-        while (items[i] < pivot) {
-            i++;
-        }
-        while (items[j] > pivot) {
-            j--;
-        }
-        if (i <= j) {
-            swap(items, i, j); //sawpping two elements
-            i++;
-            j--;
-        }
-    }
-    return i;
-}
-​
-function quickSort(items, left, right) {
-    var index;
-    if (items.length > 1) {
-        index = partition(items, left, right); //index returned from partition
-        if (left < index - 1) { //more elements on the left side of the pivot
-            quickSort(items, left, index - 1);
-        }
-        if (index < right) { //more elements on the right side of the pivot
-            quickSort(items, index, right);
-        }
-    }
-    return items;
-}
-// first call to quick sort
-var sortedArray = quickSort(items, 0, items.length - 1); */
